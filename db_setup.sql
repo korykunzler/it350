1a035b83b2fff6b681679ce648d3498d3aae5702
Kory Kunzler <korykunzler@yahoo.com>
2018-02-20 13:44:27 -0700

    DB Setup Draft

---
+++ db_setup.sql
@@ -0,0 +1,8 @@
+1 CREATE TABLE Customer (  --This is the start of the database script.
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255),
+	email VARCHAR(255),
+	address VARCHAR(255),
+	phone VARCHAR(10),
+
+)
