a49465515a4f4333f8f1630923a249bc3153cc84
Kory Kunzler <korykunzler@yahoo.com>
2018-03-14 01:04:46 -0600

    Logout page

--- logout.php
+++ logout.php
@@ -11,9 +11,11 @@
     $username = $_SESSION['login_user'];
     $connection = mysqli_connect('localhost', 'admin', 'myverysecurepassword', $database) //test:Unsecure,Iknow //connects to the database or returns the error message.
     or die("Error connecting to the MySQL server: " . mysqli_connect_error($connection));
+    $table = "Administrators";
+    
     echo $username;
     echo "<br>Session logged in set.";
-    $sqlupdate ="UPDATE 'Administrators' SET LoggedIn=0 WHERE Username='$username'";
+    $sqlupdate ="UPDATE $table SET LoggedIn=0 WHERE Username='$username'";
     echo $sqlupdate;
     echo "<br>Updating database logged in.";
     mysqli_query($connection,$sqlupdate) or die(mysqli_error($connection)); //https://coolestguidesontheplanet.com/how-to-connect-to-a-mysql-database-with-php/
