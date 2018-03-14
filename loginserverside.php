df41fe4c9bf70267d15e25befedf80f3bb3577b2
Kory Kunzler <korykunzler@yahoo.com>
2018-03-13 22:29:26 -0600

    Login security

--- loginserverside.php
+++ loginserverside.php
@@ -11,7 +11,7 @@ echo $username;
 echo $hashedpwd;
 echo "<p>Hi!</p>";
 
-/*
+
 $connection = mysqli_connect('192.168.50.34/phpmyadmin', 'admin', 'myverysecurepassword', $database) //test:Unsecure,Iknow
 or die("Error connecting to the MySQL server. TEST " + mysqli_connect_error($connection));
 
@@ -19,7 +19,7 @@ or die("Error connecting to the MySQL server. TEST " + mysqli_connect_error($con
 $database = "it350";
 $table = "Administrators";
 
-
+/*
 if(!$connection) {
   die ("Connection to server lost." . mysqli_error($connection));
 }
