c8bc29448e64bf9f4c3e4912f01cf2d1f58e30fb
Kory Kunzler <korykunzler@yahoo.com>
2018-03-13 23:21:54 -0600

    Login security

--- loginserverside.php
+++ loginserverside.php
@@ -7,8 +7,6 @@
 #echo "<p>Hello!</p>";
 $username = $_POST['username'];
 $hashedpwd = sha1($_POST['pwd']);
-echo $username;
-echo $hashedpwd;
 #echo "<p>Hi!</p>";
 
 
@@ -19,9 +17,6 @@ or die("Error connecting to the MySQL server: " . mysqli_connect_error($connecti
 $database = "it350";
 $table = "Administrators";
 
-echo "<p>Test</p>";
-
-
 if(!$connection) {
   die ("Connection to server lost." . mysqli_error($connection));
 }
@@ -31,9 +26,6 @@ $db = mysqli_select_db($connection, $database);
 if(!$db){
   die("Database could not be found." . mysqli_error($connection));
 }
-//require('connect.php');
-
-
 
 if ($connection){
     if (isset($_POST['username']) && !empty($_POST['pwd']))
@@ -44,21 +36,26 @@ if ($connection){
         $sql = "SELECT * FROM Administrators WHERE Username = 'test' AND Password = '4400c2ef10d4772936a0478f62809ed3d29db912'";
         $result = mysqli_query($db, $sql);
         $count = mysqli_num_rows($result); // returns number of rows
-        echo "Count: ";
+        echo "SQL: ";
+        echo $sql;
+        echo "<br>Count: ";
         echo $count;
-        if($count==1)
-        {
 
-	    session_start();
-            $_SESSION['login_user']= $username;
-	    $_SESSION['loggedin'] = true;
-            $sqlupdate ="UPDATE $table SET LoggedIn=1 WHERE Username='$username'";
-            mysqli_query($db,$sqlupdate) or die(mysqli_error()); //https://coolestguidesontheplanet.com/how-to-connect-to-a-mysql-database-with-php/
-		header("location:loggedin.php"); // http://www.cyberciti.biz/faq/php-redirect/
-        }else{
-        	echo "<br>Login failed. Sad day. <br>";//php page says login failed
+        if($count==1){
+	         session_start();
+           $_SESSION['login_user']= $username;
+           $_SESSION['loggedin'] = true;
+           $sqlupdate ="UPDATE $table SET LoggedIn=1 WHERE Username='$username'";
+           mysqli_query($db,$sqlupdate) or die(mysqli_error()); //https://coolestguidesontheplanet.com/how-to-connect-to-a-mysql-database-with-php/
+           header("location:loggedin.php"); // http://www.cyberciti.biz/faq/php-redirect/
+        }
+
+        else{
+        	echo "<br><br>Login failed. Sad day. <br>";//php page says login failed
           echo "Username: ";
           echo $username;
+          echo "<br>Pwd: ";
+          echo $hashedpwd;
           echo "<br>Result: ";
           echo $result;
           echo "<br>Session: ";
