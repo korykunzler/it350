3a374f01a31ecb030db837a767da13ef6053013d
Kory Kunzler <korykunzler@yahoo.com>
2018-03-13 22:21:23 -0600

    Login security

--- loginserverside.php
+++ loginserverside.php
@@ -4,7 +4,26 @@
 <p> Checking connection... </p>
 <?php
 
-require('connect.php');
+
+$connection = mysqli_connect('192.168.50.34/phpmyadmin', 'admin', 'myverysecurepassword', $database) //test:Unsecure,Iknow
+or die("Error connecting to the MySQL server. TEST " + mysqli_connect_error($connection));
+#echo $database;
+#echo $connection;
+
+$database = "it350";
+$table = "Administrators";
+
+
+if(!$connection) {
+  die ("Connection to server lost." . mysqli_error($connection));
+}
+
+$db = mysqli_select_db($connection, $database);
+
+if(!$db){
+  die("Database could not be found." . mysqli_error($connection));
+}
+//require('connect.php');
 echo "Hello!";
 
 /*
