eed4c4e53f5b5a09563246dab1904d270ec775f5
Kory Kunzler <korykunzler@yahoo.com>
2018-03-14 00:56:57 -0600

    Logout page

--- logout.php
+++ logout.php
@@ -6,15 +6,17 @@
 	</head>
 	<body>
     <?php
+    session_start();
     echo "<br>Signing the user off...";
     $username = $_SESSION['login_user'];
-
+    echo $username;
     echo "<br>Session logged in set.";
     $sqlupdate ="UPDATE $table SET LoggedIn=1 WHERE Username='$username'";
     echo "<br>Updating database logged in.";
     mysqli_query($connection,$sqlupdate) or die(mysqli_error()); //https://coolestguidesontheplanet.com/how-to-connect-to-a-mysql-database-with-php/
-    session_destroy();
     echo "<br>Session destroyed.";
+    session_destroy();
+
     echo "<br>Finished signing off. Redirecting...";
     header("location:login.php"); // http://www.cyberciti.biz/faq/php-redirect/
     exit();
