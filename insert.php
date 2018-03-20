4e44dd0f7662861bca936d0e903d148440d797ba
Kory Kunzler <korykunzler@yahoo.com>
2018-03-19 22:37:07 -0600

    Display table

--- insert.php
+++ insert.php
@@ -11,23 +11,16 @@ echo "Starting this statement.";
 include_once 'connect.php';
 
 $pname = $_POST['pname'];
-echo $pname;
 $quantity = $_POST['quantity'];
-echo $quantity;
 $numberofpieces = $_POST['numberofpieces'];
-echo $numberofpieces;
 $size = $_POST['size'];
-echo $size;
 $difficulty = $_POST['difficulty'];
-echo $difficulty;
 $theme = $_POST['theme'];
-echo $theme;
 
-echo "Finished echos";
 $sql = "INSERT INTO Puzzles (pname, quantity, numberofpieces, size, difficulty, theme) values('$pname','$quantity','$numberofpieces','$size','$difficulty','$theme')";
 $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 
-//header("location:showpuzzles.php");
+header("location:showpuzzles.php");
 
 ?>
 
