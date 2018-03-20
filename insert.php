6d595565f04ba0da05e4beb47d795a453de9ed83
Kory Kunzler <korykunzler@yahoo.com>
2018-03-19 22:08:05 -0600

    Display table

--- insert.php
+++ insert.php
@@ -1,9 +1,12 @@
+<!DOCTYPE HTML>
 <html>
+<header>
+  Inserting puzzles...
+</header>
 <body>
 
-<p>Inserting puzzles...</p>
 <?php
-
+echo "Starting this statement.";
 include_once 'connect.php';
 
 $pname = $_POST['pname'];
@@ -17,6 +20,7 @@ $theme = $_POST['theme'];
 $sql = insert into Puzzles values(pname = '$pname',quantity = '$quantity',numberofpieces = '$numberofpieces',size = '$size',difficulty = '$difficulty',theme = '$theme');
 $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 
+echo "Redirecting...";
 header("location:addpuzzles.php");
 ?>
 
