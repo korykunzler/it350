373ca2a79938648b97fbdc21cf0965477bd7517e
Kory Kunzler <korykunzler@yahoo.com>
2018-03-19 22:05:23 -0600

    Display table

--- insert.php
+++ insert.php
@@ -14,12 +14,12 @@ $difficulty = $_POST['difficulty'];
 $theme = $_POST['theme'];
 
 
-$sql = insert into Puzzles values($pname,$quantity,$numberofpieces,$size,$difficulty,$theme);
+$sql = insert into Puzzles values(pname = '$pname',quantity = '$quantity',numberofpieces = '$numberofpieces',size = '$size',difficulty = '$difficulty',theme = '$theme');
 $result = mysqli_query($connection, $sql) or die(mysqli_error($connection));
 
 header("location:addpuzzles.php");
 ?>
 
-<p>Finished!</p>
+<p>"Finished! You should never see this."</p>
 </body>
 </html>
