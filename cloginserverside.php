ebb6736d4b9b79eba6182d4f2fbb42a4f752dc76
Kory Kunzler <korykunzler@yahoo.com>
2018-03-26 14:56:45 -0600

    Customer login debugging

--- cloginserverside.php
+++ cloginserverside.php
@@ -19,9 +19,11 @@ if ($connection){ //if there is a connection, and if the username is set and the
     echo $hashedpwd;
     echo "<br>Table: "; //Echos are for debugging purposes only.
     echo $table;
+    echo "<br>";
 
         //
         $sql = "SELECT * FROM $table WHERE Username = '$username' AND Password = '$hashedpwd'"; //This is working and pulls the correct information in PHPMyAdmin
+        echo "After sql";
         $result = mysqli_query($connection, $sql) or die(mysqli_error($connection)); //START HERE. $db or $connection? NEITHER RESULT NOR COUNT APPEAR TO BE GETTING ANYTHING BACK.  //JASON'S REFERENCE CODE APPEARS TO BE DOING ALMOST THE EXACT SAME THING. NOT SURE WHAT IS UP.
         $count = mysqli_num_rows($result); // returns number of rows
 
