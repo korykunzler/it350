828a2f05b945752e67956a7dcb51eb52244de731
Kory Kunzler <korykunzler@yahoo.com>
2018-03-29 22:21:43 -0600

    Trying to display data

--- python/1star.php
+++ python/1star.php
@@ -1,5 +1,10 @@
-<?php function print1star() {
-	echo "print1star";
-	 passthru("python 1star.py");
-}
-?>
+<html>
+  <body>
+    <p> Made it here </p>
+    <?php function print1star() {
+    	echo "print1star";
+    	 passthru("python 1star.py");
+    }
+    ?>
+  </body>
+</html>
