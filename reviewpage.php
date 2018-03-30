b186156552c87c88293798483e49a4147126f630
Kory Kunzler <korykunzler@yahoo.com>
2018-03-29 21:06:46 -0600

    Adding buttons to display ratings

--- reviewpage.php
+++ reviewpage.php
@@ -22,22 +22,36 @@
 
 </form><br><br>
 
-<!--
-Need to follow one of these to set up the HTML to call the Python to access the database.
+<button onclick="print1star()">Show one star ratings</button>
+<button onclick="print2star()">Show two star ratings</button>
+<button onclick="print3star()">Show three star ratings</button>
+<button onclick="print4star()">Show four star ratings</button>
+<button onclick="print5star()">Show five star ratings</button>
+<button onclick="printall()">Show all ratings</button>
 
-https://stackoverflow.com/questions/1697484/a-button-to-start-php-script-how
-https://www.codeproject.com/Questions/780440/calling-a-function-of-php-using-html-button
-https://www.quora.com/How-do-I-create-a-PHP-button-to-run-a-script-on-a-click
+<?php function print1star() {
+	 passthru("python 1star.py");
+}
 
+function print2star() {
+	 passthru("python 2star.py");
+}
 
--->
+function print3star() {
+	 passthru("python 3star.py");
+}
 
-<?php $print1star = passthru("python 1star.py"); ?>
-<?php $print2star = passthru("python 2star.py"); ?>
-<?php $print3star = passthru("python 3star.py"); ?>
-<?php $print4star = passthru("python 4star.py"); ?>
-<?php $print5star = passthru("python 5star.py"); ?>
-<?php $printall = passthru("python printall.py"); ?>
+function print4star() {
+	 passthru("python 4star.py");
+}
+
+function print5star() {
+	 passthru("python 5star.py");
+}
+
+function printall() {
+	 passthru("python printall.py");
+} ?>
 
 
 
@@ -48,5 +62,14 @@ Here display the reviews along with the average of them all (trigger)
 Possible display articles
 https://stackoverflow.com/questions/31766750/display-all-document-on-php
 http://php.net/manual/en/mongo.tutorial.cursor.php
+
+-->
+<!--
+Need to follow one of these to set up the HTML to call the Python to access the database.
+
+https://stackoverflow.com/questions/1697484/a-button-to-start-php-script-how
+https://www.codeproject.com/Questions/780440/calling-a-function-of-php-using-html-button
+https://www.quora.com/How-do-I-create-a-PHP-button-to-run-a-script-on-a-click
+
 
 -->
