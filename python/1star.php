819a3597b549a47bdce3796327337902baacb466
Kory Kunzler <korykunzler@yahoo.com>
2018-03-29 22:31:13 -0600

    Trying to display data

--- python/1star.php
+++ python/1star.php
@@ -1,8 +1,8 @@
 <html>
   <body>
     <p> Made it here </p>
-    <?php 
-    	echo "print1star";
+    <?php
+    	echo "print1star<br>";
     	 passthru("python 1star.py");
     ?>
     <p> How about here? </p>
