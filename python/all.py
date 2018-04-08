61efdc9d783474a033f53a034557df43ab6b42e5
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 21:10:09 -0600

    Tweaking a few settings.

--- python/all.py
+++ python/all.py
@@ -1,23 +1,23 @@
-print("**Python printall**")
+#print("**Python printall**")
 from pymongo import MongoClient
-print("here one <br>")
+#print("here one <br>")
 client = MongoClient('localhost', 27017)
-print("here 2 <br>")
-print("here three <br>")
+#print("here 2 <br>")
+#print("here three <br>")
 db = client['ratingsdb']
 coll = db['ratingsdb']
-print("I am here now<br>")
-print(db, "<db,coll>", coll)
+#print("I am here now<br>")
+#print(db, "<db,coll>", coll)
 printall = coll.find()
 print("*************<br><br>")
 for p in printall:
-    print("Test<br>")
+    #print("Test<br>")
     print p
 
 #onestar = "col.find({'rating': '1'})"
-print("<br>How about now?<br>")
+#print("<br>How about now?<br>")
 #print(onestar)
-print("<br> End of python <br>")
+#print("<br> End of python <br>")
 
 #http://api.mongodb.com/python/current/tutorial.html#making-a-connection-with-mongoclient
 #https://stackoverflow.com/questions/19781768/executing-python-script-with-php-variables
