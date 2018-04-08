20b5a0823d9b1fc6f8354253f5e6d67c6f727246
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 22:13:05 -0600

    Tweaking a few settings.

--- python/insert.php
+++ python/insert.php
@@ -15,7 +15,7 @@
       $selection = $_POST['ratingselection'];
       echo($selection);
       $selection ="5";
-    	 passthru("python insert.py, $username, $selection");
+    	 passthru("python insert.py, sallsue, 5");
        echo($username);
        echo("<--<br>-->");
        echo($selection);
