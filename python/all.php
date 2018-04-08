e393476734801e66ddc3de09b5847739a18d3a2b
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 19:56:59 -0600

    Tweaking a few settings.

---
+++ python/all.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <p> Made it to all.php </p>
+    <?php
+    	echo "printall<br>";
+    	 passthru("python all.py");
+    ?>
+    <p> Back to php after python </p>
+  </body>
+</html>
