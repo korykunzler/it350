fb620b00ff037b84273dff428c6b77f4f2836427
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 22:22:40 -0600

    Tweaking a few settings.

--- python/insert.py
+++ python/insert.py
@@ -3,19 +3,12 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client['ratingsdb']
 coll = db['ratingsdb']
-print("testing")
-print(sys.argv[1])
-print("<<br>>")
-print(sys.argv[1])
-print("<br>Made it here")
-username = sys.argv[1]
-rating = sys.argv[2]
 
 print("Now here")
 print(username, "<br>")
 print(rating, "<br>")
-
-coll.insertOne({username:"username", rating: "rating"})
+print("About to insert")
+coll.insert({username:"username", rating: "rating"})
 print("<br>After insert statement")
 #print("Starting printout... <br><br>")
 #or p in printall:
