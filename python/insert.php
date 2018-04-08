e9744a109afd3ecb21dcf653f27b80d0ffdbd919
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 21:45:43 -0600

    Tweaking a few settings.

--- python/insert.php
+++ python/insert.php
@@ -1,9 +1,19 @@
 <html>
   <body>
     <p> Made it to insert.php </p>
+    <?php // http://stackoverflow.com/questions/1545357/how-to-check-if-a-user-is-logged-in-in-php // need to double check this to be sure it won't let them here without logging in.
+      session_start();
+      if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+          #echo "Thanks for logging in, " . $_SESSION['login_user'] . "!";
+      } else {
+          header("Location: clogin.php");
+        }
+    ?>
+    <p> Past login check </p>
     <?php
-    	//echo "printall php<br>";
-    	 passthru("python insert.py ");
+      $username = $_SESSION['login_user']; //Receives the username/pwd from the post and saves them to variables
+      $selection = ($_POST['ratingselection']);
+    	 passthru("python insert.py, $username, $selection");
     ?>
     <p> Back to php after python </p>
   </body>
