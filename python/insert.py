b09cfc30e776f0cbfc82de5c30a164d0d5deb99b
Kory Kunzler <korykunzler@yahoo.com>
2018-04-07 21:31:22 -0600

    Tweaking a few settings.

--- python/insert.py
+++ python/insert.py
@@ -3,9 +3,15 @@ client = MongoClient('localhost', 27017)
 db = client['ratingsdb']
 coll = db['ratingsdb']
 
-printall = coll.find()
-print("Starting printout... <br><br>")
-for p in printall:
-    print p
-    print("<br>")
-print("<br>End of printout")
+username = sys.argv[1]
+rating = sys.argv[2]
+
+print(username, "<br>")
+print(rating, "<br>")
+
+#printall = coll.find()
+#print("Starting printout... <br><br>")
+#or p in printall:
+#    print p
+#    print("<br>")
+#print("<br>End of printout")
